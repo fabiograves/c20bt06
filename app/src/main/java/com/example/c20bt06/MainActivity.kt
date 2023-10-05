998141cc2bceb275c06a142d9b346472178b91f8
Binho <fabioegraves@gmail.com>
2023-10-05 17:24:22 -0300

    0.01

---
+++ app/src/main/java/com/example/c20bt06/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.c20bt06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.akexorcist.bluetoothclassic.BluetoothClassic
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
